44d59e72a54adc0217471e3ad575a224806c5668
Lucas Arnström <lucas@arnstrom.se>
2017-06-03 12:40:05 +0200

    annotated category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170602201619) do
+ActiveRecord::Schema.define(version: 20170603103719) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
