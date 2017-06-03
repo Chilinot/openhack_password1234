8b4a49008b158ff3bc9a7e2055a0595330110413
Lucas Arnström <lucas@arnstrom.se>
2017-06-03 22:55:50 +0200

    added ability to create new skills

--- app/controllers/skills_controller.rb
+++ app/controllers/skills_controller.rb
@@ -13,4 +13,22 @@ class SkillsController < ApplicationController
   def show
     @skill = Skill.find(params[:id])
   end
+
+  def new
+    @skill = Skill.new
+  end
+
+  def create
+    @skill = Skill.new(skill_params)
+    if @skill.save
+      redirect_to skills_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+    def skill_params
+      params.require(:skill).permit(:title, :description)
+    end
 end
